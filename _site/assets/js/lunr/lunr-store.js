60604e9ddf0c5e5729ee6a973750ca8da0109a02
hydthemoon <40457043+hydthemoon@users.noreply.github.com>
2022-07-07 23:49:07 +0900

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["etc"],
         "url": "/misc/hello-blog/",
         "teaser": null
+      },{
+        "title": "2022 상반기 회고",
+        "excerpt":"상반기 목표 대략 3월쯤 세운 상반기 기존 목표는 아래와 같다. ML 스터디 성실히 진행 학점 4 이상 코포 시작, 퍼플 레이팅 만들기 (올해 내로) 건강 관리 4점 이상 돈 관리 상반기 결산 결과적으로 6월까지 해온 것들은 다음과 같다. ML 스터디 진행중 단단한 머신러닝 완주 우아한 스터디에서 PRML 진행중 학점 4.1...","categories": ["misc"],
+        "tags": ["etc"],
+        "url": "/misc/2022-%EC%83%81%EB%B0%98%EA%B8%B0-%ED%9A%8C%EA%B3%A0/",
+        "teaser": null
       }]
