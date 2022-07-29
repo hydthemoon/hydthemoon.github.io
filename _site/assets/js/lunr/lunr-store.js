33ab9fb08e1aaaa6c327c00d9c3914a0c1ea2d57
hydthemoon <40457043+hydthemoon@users.noreply.github.com>
2022-07-29 16:32:55 +0900

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,42 +8,48 @@ var store = [{
         "title": "2022 상반기 회고",
         "excerpt":"상반기 목표 대략 3월쯤 세운 상반기 기존 목표는 아래와 같다. ML 스터디 성실히 진행 학점 4 이상 코포 시작, 퍼플 레이팅 만들기 (올해 내로) 건강 관리 4점 이상 돈 관리 상반기 결산 결과적으로 6월까지 해온 것들은 다음과 같다. ML 스터디 진행중 단단한 머신러닝 완주 우아한 스터디에서 PRML 진행중 학점 4.1...","categories": ["misc"],
         "tags": ["etc"],
-        "url": "/misc/2022-%EC%83%81%EB%B0%98%EA%B8%B0-%ED%9A%8C%EA%B3%A0/",
+        "url": "/misc/2022-fisthalf-rem/",
         "teaser": null
       },{
         "title": "인간실격",
         "excerpt":"후기 전개가 어지러워서 정신을 차리고 읽느라 애를 먹었다. 호밀밭의 파수꾼을 다른 버전으로 읽는 느낌을 받았고 일본 소설만의 표현하기 어려운 그 특유의 분위기가 조금은 불쾌하기도 했다. 주인공 요조는 그야말로 염세주의에 제대로 빠진 사람으로 보였는데, 낙관주의에 가까운 나로서는 그의 가정환경과 일련의 행동들이 그저 안타깝게 보일 뿐이었다. 요조의 허무주의는 분명 패전으로 무너진 일본인들을...","categories": ["book"],
         "tags": ["novel"],
-        "url": "/book/%EC%9D%B8%EA%B0%84%EC%8B%A4%EA%B2%A9/",
+        "url": "/book/Book-no-longer-human/",
         "teaser": null
       },{
         "title": "유산소 인터벌 도입",
         "excerpt":"개인 유산소 운동은 페이스 유지하면서 달리는 일반적인 조깅만 해왔었는데, 최근에 인터벌을 도입하고 오늘 2번째로 뛰어보았다. 효과가 있는지는 풋살할 때 확인할 수 있겠지만, 일단 너무 힘듬.. 원래 인터벌하시는 분들 보면 전속력-천천히를 한세트로 10번 정도 조지시던데 나는 5세트정도에 뻗어버림; 전속력이 진짜 전속력이 아닌건지 천천히를 진짜 완전 천천히로 해야하는지 뭔가 잘못된거 같기도 한데,...","categories": ["운동"],
         "tags": ["유산소"],
-        "url": "/%EC%9A%B4%EB%8F%99/%EC%9D%B8%ED%84%B0%EB%B2%8C-%ED%9E%98%EB%93%9C%EB%84%A4/",
+        "url": "/%EC%9A%B4%EB%8F%99/interval/",
         "teaser": null
       },{
         "title": "[PRML] 11장 표집법",
         "excerpt":"개요 10장에서는 결정적 근사를 기반으로한 추론 알고리즘을 배웠다면, 이번 장에서는 수치적 표집법에 기반을 둔 근사 추론법인 Monte Carlo 테크닉에 대해 다룬다. 운이 좋다면 latent variable에 대한 사후분포를 직접적으로 구할 수 있겠지만 사실은 대부분의 경우 분포의 기댓값 정보만 필요로 한다. 따라서 이번 장에서는 기댓값만 관심을 가져보자. 연속 변수일 때는 다음과 같이...","categories": ["ML"],
         "tags": ["PRML","표집법","MCMC","깁스 샘플링"],
-        "url": "/ml/PRML-%ED%91%9C%EC%A7%91%EB%B2%95/",
+        "url": "/ml/PRML-11-sampling-methods/",
         "teaser": null
       },{
         "title": "[JAVA] 클래스/인스턴스 변수/메서드의 구분",
         "excerpt":"코테 언어 3대장 중 초심자가 봤을 때 가장 어려워보이는 언어는 자바가 아닐까 싶다. C 조금 하다가 파이썬 원툴로 잘 살아왔지만 회사에서 자바를 쓰기 때문에.. 그리고 앞으로 자바민국에서 살아가기 위해 나도 자바 공부를 시작했는데, 이것도 정리하면서 공부하면 용어를 좀더 오래 기억하는데도움이 되지 않을까 싶어 정리해보기로 했다. 필드에서의 구분 class variable (클래스...","categories": ["JAVA"],
         "tags": ["JAVA","cs"],
-        "url": "/java/%EC%9E%90%EB%B0%94%EA%B3%B5%EB%B6%80/",
+        "url": "/java/Java-study/",
         "teaser": null
       },{
         "title": "인간관계의 법칙",
         "excerpt":"후기   .   추천하는 독자   추천하지 않는 독자   all  ","categories": ["book"],
         "tags": ["social"],
-        "url": "/book/%EC%9D%B8%EA%B0%84-%EA%B4%80%EA%B3%84%EC%9D%98-%EB%B2%95%EC%B9%99/",
+        "url": "/book/Book-the-art-of-seduction/",
         "teaser": null
       },{
         "title": "[JAVA] 메모리 관리 - Garbage Collection",
         "excerpt":"Unreachable Object들은 Garbage Collector에 의해 회수된다. 위의 내용을 이해하기 위해 JAVA에서의 메모리 관리 방법에 대해 살펴보자. 아래 reference들의 흐름을 따라 살을 붙이는 방식으로 작성했다. 일단 간략하게 stack과 heap의 특성을 요약해보자. Stack Heap에 할당된 object들의 reference가 할당된다. value type(primitive type)들이 직접 저장된다. stack의 변수들은 특정한 visibility(scope)가 있다. Thread마다 독립 stack 공간을...","categories": ["JAVA"],
         "tags": ["Garbage Colletion","cs"],
-        "url": "/java/%EC%9E%90%EB%B0%94-%ED%9E%99-%EC%8A%A4%ED%83%9D/",
+        "url": "/java/Java-heap-stack/",
+        "teaser": null
+      },{
+        "title": "알고리즘에서의 고속 푸리에 변환",
+        "excerpt":"Fourier Series &amp; Transfrom Review 보통 이공계 학부생이라면 2학년 즈음에 신호및시스템 수업 혹은 복소함수론 수업에서 푸리에 급수와 변환에 대해 배웠을 것이다. 주인장의 경우 2학년 2학기 때 두 과목을 같이 들었는데, 수학과에서 열린 복소함수론 수업 초반에 Fourier Analysis하면서 좀 보고, 신호 수업에서 계산을 미친듯이 많이 했던 기억이 난다. 어쨋든, Fourier Transform을...","categories": ["algo"],
+        "tags": ["FFT","신호및시스템","복소함수론","백준"],
+        "url": "/algo/Algo-FFT/",
         "teaser": null
       }]
